e90eb47910141c67f28c9fe3481ace0963599779
Caio <dandinzin@gmail.com>
2025-11-25 21:00:34 -0300

    feat: new home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container>
+    <h1>Portfolio Manager Frontend</h1>
+    <p>Interface para executar o modelo de seleção de ações e gerenciar cadastro de stocks.</p>
+  </v-container>
 </template>
+
+<script setup lang="ts"></script>
