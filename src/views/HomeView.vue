fbcf0c38faaa95fc85eab542b00e3b29499cf3fd
Caio <dandinzin@gmail.com>
2025-12-13 11:03:15 -0300

    feat: tela home

--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-container>
-    <h1>Portfolio Manager Frontend</h1>
-    <p>Interface para executar o modelo de seleção de ações e gerenciar cadastro de stocks.</p>
-  </v-container>
-</template>
-
-<script setup lang="ts"></script>
