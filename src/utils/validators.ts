2f91061717acac43ba6a14e49191e8066b955d14
Caio <dandinzin@gmail.com>
2025-12-12 22:22:12 -0300

    feat: adicionando validacao de acao registrada

---
+++ src/utils/validators.ts
@@ -0,0 +1,9 @@
+export function isValidStockSymbol(symbol: string): boolean {
+  if (!symbol) return false
+
+  const normalized = symbol.trim().toUpperCase()
+
+  const regex = /^[A-Z]{4}[0-9]{1,2}?$/
+
+  return regex.test(normalized)
+}
--- src/views/AddSymbol/index.vue
+++ src/views/AddSymbol/index.vue
@@ -3,8 +3,8 @@
     <v-card width="450" class="pa-6" elevation="8">
       <h2 class="text-h5 mb-4 text-center">Cadastrar Novo Símbolo</h2>
 
-      <v-text-field v-model="symbol" label="Símbolo da Ação (ex: PETR4)" variant="outlined" density="comfortable"
-        prepend-inner-icon="mdi-format-letter-case" />
+      <v-text-field v-model="symbol" label="Símbolo da Ação (ex: PETR4)" :error="symbolError"
+        error-message="Formato inválido" variant="outlined" />
 
       <v-text-field v-model="createdById" label="ID do Usuário" type="number" variant="outlined" density="comfortable"
         prepend-inner-icon="mdi-account" />
@@ -19,37 +19,52 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { createStock } from "../../services/stocks";
-import { notify } from '@/utils/toast'
+import { notify } from '@/utils/toast';
+import { isValidStockSymbol } from '@/utils/validators';
+import { computed } from 'vue';
 
 const symbol = ref("");
 const createdById = ref<number | null>(null);
 const loading = ref(false);
+
+const symbolError = computed(() =>
+  symbol.value.length > 0 && !isValidStockSymbol(symbol.value)
+);
 
 async function handleSubmit() {
   loading.value = true;
 
   try {
     if (!symbol.value || !createdById.value) {
-      notify.error('Preencha todos os campos.')
-      loading.value = false;
+      notify.error('Preencha todos os campos.');
       return;
     }
 
-    await createStock(symbol.value.trim(), createdById.value);
+    const normalizedSymbol = symbol.value.trim().toUpperCase()
 
-    notify.success('Símbolo registrado com sucesso!')
-    symbol.value = "";
+    if (!isValidStockSymbol(normalizedSymbol)) {
+      notify.warning(
+        'Símbolo inválido. Use apenas letras e números (ex: PETR4, AAPL).'
+      );
+      return;
+    }
+
+    await createStock(normalizedSymbol, createdById.value);
+
+    notify.success('Símbolo registrado com sucesso!');
+    symbol.value = '';
     createdById.value = null;
   } catch (error: any) {
     if (error?.response?.status === 409) {
-      notify.error('Símbolo já existe!')
+      notify.error('Símbolo já existe!');
     } else {
-      notify.error('Erro ao registrar símbolo.')
+      notify.error('Erro ao registrar símbolo.');
     }
   } finally {
     loading.value = false;
   }
 }
+
 </script>
 
 <style scoped>
