ed34092875a4feb94a3cc2cac6a52696b9249336
Caio <dandinzin@gmail.com>
2026-02-08 19:27:43 -0300

    feat: corrigindo rota de register

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const router = createRouter({
     },
     {
       path: '/Register',
-      name: 'Register',
+      name: 'legister',
       component: () => import('../views/Register/index.vue'),
     },
     {
