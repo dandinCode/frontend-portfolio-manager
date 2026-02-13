7c7c8cac9f553d996634ca0ca941693db1a8cfce
Caio <dandinzin@gmail.com>
2026-02-12 22:50:53 -0300

    feat: add componente de pesquisa

---
+++ src/components/StockSearch.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-text-field v-model="search" label="Buscar ação (ex: PETR, ITUB)" prepend-inner-icon="mdi-magnify"
+        variant="outlined" density="comfortable" clearable color="blue-lighten-2" class="mb-4" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStocksStore } from '@/stores/stocksStore'
+
+const stocks = useStocksStore()
+
+const search = computed({
+    get: () => stocks.searchTerm,
+    set: (value: string) => stocks.setSearch(value),
+})
+</script>
