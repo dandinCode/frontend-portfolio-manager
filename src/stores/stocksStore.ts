9f180517e271d600844f9c0c939787e13cbd5f63
Caio <dandinzin@gmail.com>
2026-02-12 22:50:40 -0300

    feat: add function de pesquisa

--- src/stores/stocksStore.ts
+++ src/stores/stocksStore.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { getStockSymbols } from '@/services/stocks'
 import type { StockSymbol } from '@/types/types'
 
 export const useStocksStore = defineStore('stocksStore', () => {
   const symbols = ref<StockSymbol[]>([])
   const loading = ref(false)
+  const searchTerm = ref('')
 
   async function loadStocks() {
     loading.value = true
@@ -16,9 +17,24 @@ export const useStocksStore = defineStore('stocksStore', () => {
     }
   }
 
+  function setSearch(term: string) {
+    searchTerm.value = term
+  }
+
+  const filteredSymbols = computed(() => {
+    if (!searchTerm.value) return symbols.value
+
+    const term = searchTerm.value.toLowerCase()
+
+    return symbols.value.filter((stock) => stock.symbol.toLowerCase().includes(term))
+  })
+
   return {
     symbols,
+    filteredSymbols,
     loading,
+    searchTerm,
+    setSearch,
     loadStocks,
   }
 })
