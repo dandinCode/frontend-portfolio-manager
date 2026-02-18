526ea8a642026ff66cb1b11fac81ad527891c2b4
Caio <dandinzin@gmail.com>
2026-02-18 14:51:06 -0300

    feat: fazendo request pra salvar portfolio

--- src/services/analisys.ts
+++ src/services/analisys.ts
@@ -1,12 +1,18 @@
-import type { PortfolioAnalysis } from '../types/types';
-import { analysisApi } from './http/analysisApi';
+import type { PortfolioAnalysis, SavePortfolioPayload } from '../types/types'
+import { analysisApi } from './http/analysisApi'
 
 export async function analyzeStocks(portfolioAnalysis: PortfolioAnalysis) {
   try {
-    const res = await analysisApi.post('/analysis/analyze', portfolioAnalysis);
-    return res.data;
+    const res = await analysisApi.post('/analysis/analyze', portfolioAnalysis)
+    return res.data
   } catch (error) {
-    console.error('Erro ao analisar ações:', error);
-    throw error;
+    console.error('Erro ao analisar ações:', error)
+    throw error
   }
+}
+
+export async function savePortfolio(payload: SavePortfolioPayload) {
+  console.log(payload)
+  const { data } = await analysisApi.post('/portfolios', payload)
+  return data
 }
