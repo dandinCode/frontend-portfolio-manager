241ad12af2164590b42887c1420e7cc328795b97
Caio <dandinzin@gmail.com>
2026-02-23 21:48:10 -0300

    fix: corrigindo rota da request

--- src/services/portfolio.ts
+++ src/services/portfolio.ts
@@ -2,6 +2,6 @@ import type { SavePortfolioPayload } from '../types/types'
 import { analysisApi } from './http/analysisApi'
 
 export async function savePortfolio(payload: SavePortfolioPayload) {
-  const { data } = await analysisApi.post('/portfolios', payload)
+  const { data } = await analysisApi.post('/portfolio', payload)
   return data
 }
