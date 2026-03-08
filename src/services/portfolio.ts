3e924c1ca887f8d02d6f85ff3b3cda335d4c3dd2
Caio <dandinzin@gmail.com>
2026-03-08 11:07:44 -0300

    feat: criando requisicoes de portfolio

--- src/services/portfolio.ts
+++ src/services/portfolio.ts
@@ -5,3 +5,11 @@ export async function savePortfolio(payload: SavePortfolioPayload) {
   const { data } = await api.post('/portfolio', payload)
   return data
 }
+
+export function getMyPortfolios() {
+  return api.get("/portfolio")
+}
+
+export function removePortfolio(id: number) {
+  return api.delete(`/portfolio/${id}`)
+}
