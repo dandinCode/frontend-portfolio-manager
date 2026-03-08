d3b3837941748d25a68d31588dc82e36571cc9bc
Caio <dandinzin@gmail.com>
2026-03-08 11:07:07 -0300

    feat: passando token na interceptor

--- src/services/http/api.ts
+++ src/services/http/api.ts
@@ -8,4 +8,14 @@ export const api = axios.create({
   timeout: 10_000,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
 setupInterceptors(api);
