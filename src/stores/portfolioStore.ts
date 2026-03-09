5c680a111a4a30ab1f39a3cbb6e11296fd408981
Caio <dandinzin@gmail.com>
2026-03-08 21:24:21 -0300

    feat: crate store portfolio

---
+++ src/stores/portfolioStore.ts
@@ -0,0 +1,56 @@
+import { defineStore } from 'pinia'
+import { getMyPortfolios } from '@/services/portfolio'
+
+export const usePortfolioStore = defineStore('portfolio', {
+  state: () => ({
+    portfolios: [] as any[],
+    loaded: false,
+  }),
+
+  actions: {
+    loadFromSession() {
+      const stored = sessionStorage.getItem('portfolios')
+
+      if (stored) {
+        this.portfolios = JSON.parse(stored)
+        this.loaded = true
+      }
+    },
+
+    saveToSession() {
+      sessionStorage.setItem('portfolios', JSON.stringify(this.portfolios))
+    },
+
+    async fetchPortfolios() {
+      if (this.loaded) return
+
+      const { data } = await getMyPortfolios()
+
+      this.portfolios = data
+
+      this.loaded = true
+
+      this.saveToSession()
+    },
+
+    setPortfolios(portfolios: any[]) {
+      this.portfolios = portfolios
+      this.saveToSession()
+    },
+
+    addPortfolio(portfolio: any) {
+      this.portfolios.push(portfolio)
+      this.saveToSession()
+    },
+
+    removePortfolio(id: number) {
+      this.portfolios = this.portfolios.filter((p) => p.id !== id)
+
+      this.saveToSession()
+    },
+
+    getPortfolio(id: number) {
+      return this.portfolios.find((p) => p.id === id)
+    },
+  },
+})
